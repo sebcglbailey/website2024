724f6a96757e2f7555b6393c130b147c86add1a6
Sebastian Bailey <sebcglbailey@gmail.com>
2025-02-12 12:32:57 +0000

    adds device size page back in

---
+++ portfolio-site/app/device-size/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+
+import styles from "./device-size.module.css";
+
+// Hook
+const getFullWidth = () => screen?.width
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+const getFullHeight = () => screen?.height
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+const getInnerWidth = () => window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+const getInnerHeight = () => window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+function useCurrentSize() {
+    // save current window width in the state object
+    let [fullWidth, setFullWidth] = useState(getFullWidth());
+    let [fullHeight, setFullHeight] = useState(getFullHeight());
+    let [innerWidth, setInnerWidth] = useState(getInnerWidth());
+    let [innerHeight, setInnerHeight] = useState(getInnerHeight());
+
+    // in this case useEffect will execute only once because
+    // it does not have any dependencies.
+    useEffect(() => {
+        const resizeListener = () => {
+            // change size from the state object
+            setFullWidth(getFullWidth())
+            setFullHeight(getFullHeight())
+            setInnerWidth(getInnerWidth())
+            setInnerHeight(getInnerHeight())
+        };
+        // set resize listener
+        window.addEventListener('resize', resizeListener);
+
+        // clean up function
+        return () => {
+            // remove resize listener
+            window.removeEventListener('resize', resizeListener);
+        }
+    }, [])
+
+    return { innerWidth, innerHeight, fullWidth, fullHeight };
+}
+
+export default function Page() {
+    
+    const size = useCurrentSize();
+    console.log(size)
+
+    return (
+        <div className={styles.deviceContainer}>
+            <h2>Window size:</h2>
+            <p>{size.innerWidth} x {size.innerHeight}</p>
+            <br/>
+            <h2>Screen size:</h2>
+            <p>{size.fullWidth} x {size.fullHeight}</p>
+        </div>
+
+    )
+
+}
